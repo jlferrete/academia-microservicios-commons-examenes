b347e454d65bed68727904038342bf60321d02b2
JlFerrete <joseluisferrete@gmail.com>
2020-07-13 08:33:01 +0200

    Creating commons library to reuse entity classes of exams

---
+++ src/main/java/com/formacionbdi/microservicios/commons/examenes/models/entity/Examen.java
@@ -0,0 +1,95 @@
+package com.formacionbdi.microservicios.commons.examenes.models.entity;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="examenes")
+public class Examen {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nombre;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="create_at")
+	private Date createAt;
+	
+	@JsonIgnoreProperties(value = {"examen"}, allowSetters = true)
+	@OneToMany(mappedBy = "examen", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Pregunta> preguntas;
+	
+	
+	
+	public Examen() {
+		this.preguntas = new ArrayList<>();
+	}
+
+	@PrePersist
+	public void prePersist() {
+		this.createAt = new Date();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	public List<Pregunta> getPreguntas() {
+		return preguntas;
+	}
+
+	public void setPreguntas(List<Pregunta> preguntas) {
+		this.preguntas.clear();
+		preguntas.forEach(this::addPregunta);
+		
+	}
+	
+	public void addPregunta(Pregunta pregunta) {
+		this.preguntas.add(pregunta);
+		pregunta.setExamen(this);
+	}
+	
+	public void removePregunta(Pregunta pregunta) {
+		this.preguntas.remove(pregunta);
+		pregunta.setExamen(null);
+	}
+	
+}
