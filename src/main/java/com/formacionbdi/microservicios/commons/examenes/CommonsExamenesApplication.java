ad003a5a3c6c14506638acd39b117be6728bd4b0
JlFerrete <joseluisferrete@gmail.com>
2020-07-13 08:14:07 +0200

    Initial commit

---
+++ src/main/java/com/formacionbdi/microservicios/commons/examenes/CommonsExamenesApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.microservicios.commons.examenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonsExamenesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonsExamenesApplication.class, args);
+	}
+
+}
